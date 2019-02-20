d8f87a387d4ce43c78a23fd7a94f582bbe3dbdff
chrys <chrys@linux-a11y.org>
2019-02-20 16:42:28 +0100

    Update fileManager.py

--- src/dragonfilemanager/core/fileManager.py
+++ src/dragonfilemanager/core/fileManager.py
@@ -6,12 +6,30 @@ class fileManager():
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
     def getInfo(self, fullPath):
+        if fullPath == '':
+            return None
+        if not os.path.exists(fullPath):
+            return None
         # basic
         name = os.path.basename(fullPath)
         path = os.path.dirname(fullPath)
         entry = {'name': name,
          'full': fullPath,
          'path': path,
+         'marked': False,
+         'type': None,
+         'mode', None,
+         'ino', None,
+         'dev', None,
+         'nlink', None,
+         'uid', None,
+         'uname': None,
+         'gid', None,
+         'gname': None,
+         'size', None,
+         'atime', None,
+         'mtime', None,
+         'ctime', None,
         }
         # type
         if os.path.isfile(fullPath):
@@ -26,13 +44,21 @@ class fileManager():
         info = None
         try:
             info = os.stat(fullPath)
+            entry['mode'] = info.st_mode
+            entry['ino'] = 2 ** 32 + info.st_ino + 1  # Ensure st_ino > 2 ** 32
+            entry['dev'] = info.st_dev
+            entry['nlink'] = info.st_nlink
+            entry['uid'] = info.st_uid
+            entry['uname'] = username
+            entry['gid'] = info.st_gid
+            entry['gname'] = groupname
+            entry['size'] = info.st_size
+            entry['atime'] = info.st_atime
+            entry['mtime'] = info.st_mtime
+            entry['ctime'] = info.st_ctime
         except:
-            info = None
-        if info:
-            try:
-                entry['info'] = info
-            except:
-                pass
+            pass
+        return entry.copy()
     def openFile(self, fullPath):
         try:
             os.execlp('nano', 'nano', fullPath)
