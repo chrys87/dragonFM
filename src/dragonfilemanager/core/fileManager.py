eb012912d14b88bc81db77890129e6f391590e4f
chrys <chrys@linux-a11y.org>
2019-02-20 15:24:31 +0100

    Create fileManager.py

---
+++ src/dragonfilemanager/core/fileManager.py
@@ -0,0 +1,7 @@
+import sys, os, curses
+
+class fileManager():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
