f254b4e63832af15b31f0b0fc1d84aa73a1780d0
chrys <chrys@linux-a11y.org>
2019-02-20 15:37:10 +0100

    Update fileManager.py

--- src/dragonfilemanager/core/fileManager.py
+++ src/dragonfilemanager/core/fileManager.py
@@ -5,3 +5,33 @@ class fileManager():
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
+    def getInfo(fullPath):
+        info = None
+        try:
+            info = os.stat(fullPath)
+        except:
+            pass
+        entry = {'name': e,
+         'full': fullPath,
+         'path': path,
+         'info': info
+        }
+        if os.path.isfile(fullPath):
+            entry['type'] = 'file'
+        elif os.path.isdir(fullPath):
+            entry['type'] = 'directory'
+        elif os.path.islink(fullPath):
+            entry['type'] = 'link'
+        elif os.path.ismount(fullPath):
+            entry['type'] = 'mountpoint'
+        '''
+        / 	Directory
+        * 	Executable
+        | 	Fifo
+        = 	Socket
+        @ 	Symbolic Link
+        @/ 	Symbolic Link to directory
+        b 	Block Device
+        c 	Character Device
+        ? 	Unknown
+        '''
