0ae65c288cb761e7e6cfc87c32a6af6e17865d1c
chrys <chrys@linux-a11y.org>
2019-02-21 16:45:10 +0100

    Update viewManager.py

--- src/dragonfilemanager/core/viewManager.py
+++ src/dragonfilemanager/core/viewManager.py
@@ -6,68 +6,37 @@ from dragonfilemanager.core import tabManager
 
 class viewManager():
     def __init__(self, dragonfmManager):
+        self.mode = 0 # 0: tab, 1: Main Menu
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-        self.tabs = {}
-        self.mainMenu = mainMenuManager.mainMenuManager(self.dragonfmManager)
-        self.currentTab = -1
-        self.mode = 0 # 0: tab, 1: Main Menu
-        self.addTab()
-    def addTab(self, changeToNew = True):
-        if self.mode != 0:
-            return
-        id = len(self.tabs)
-        self.tabs[id] = folderManager.folderManager(id, self.dragonfmManager)
-        if changeToNew:
-            self.changeTab(id)
+        self.tabManager = tabManager.tabManager(self.dragonfmManager)
+        self.mainMenuManager = mainMenuManager.mainMenuManager(self.dragonfmManager)
 
-    def closeTab(self, tab):
-        if self.mode != 0:
-            return
-        if len(self.tabs) > 1:
-            if tab == self.currentTab:
-                self.changeTab(0)
-            del(self.tabs[tab])
     def changeMode(self, mode):
         if self.mode == mode:
             return
         if self.mode == 0:
-            self.tabs[self.currentTab].leave()
+            self.tabManager.leave()
         elif self.mode == 1:
-            self.mainMenu.leave()
+            self.mainMenuManager.leave()
 
         if mode == 0:
-            self.tabs[self.currentTab].enter()
+            self.tabManager.enter()
         elif mode == 1:
-            self.mainMenu.enter()
-
+            self.mainMenuManager.enter()
         self.mode = mode
-
-    def changeTab(self, currentTab):
-        if self.mode != 0:
-            return
-        if self.currentTab == currentTab:
-            return
-        if currentTab < 0:
-            currentTab = len(self.tabs) - 1
-        if currentTab >= len(self.tabs):
-            currentTab = 0
-        # old
-        if self.currentTab != -1:
-            self.tabs[self.currentTab].leave()
-        # new
-        self.tabs[currentTab].enter()
-        self.currentTab = currentTab
 
     def update(self):
         if self.mode == 0:
-            self.tabs[self.currentTab].draw()
+            self.tabManager.draw()
         elif self.mode == 1:
-            self.mainMenu.draw()
-
+            self.mainMenuManager.draw()
+    def handleViewInput(self, key):
+        return False
     def handleInput(self, key):
-        if self.mode == 0:
-            self.tabs[self.currentTab].handleInput(key)
-        elif self.mode == 1:
-            self.mainMenu.handleInput(key)
+        if not self.handleViewInput(key):
+            if self.mode == 0:
+                self.tabManager.handleInput(key)
+            elif self.mode == 1:
+                self.mainMenuManager.handleInput(key)
