55af2a264ade7986721e5e1d64b33b604c71fb0e
chrys <chrys@linux-a11y.org>
2019-03-05 16:26:55 +0100

    Create goto_home.py

---
+++ src/dragonfilemanager/commands/folder/goto_home.py
@@ -0,0 +1,26 @@
+from os.path import expanduser
+
+class command():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
+        self.viewManager = None
+    def shutdown(self):
+        pass
+    def getName(self):
+        return _('Refresh')
+    def getDescription(self):
+        return _('Refresh current folder')
+    def active(self):
+        return True
+    def getValue(self):
+        return None
+    def getShortcut(self):
+        return None
+    def run(self, callback = None):
+        if self.viewManager == None:
+            self.viewManager = self.dragonfmManager.getViewManager()
+        self.viewManager.getCurrentTab().gotoFolder(expanduser("~"))
+        if callback:
+            callback()
