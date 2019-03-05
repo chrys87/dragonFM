c5b4af4480745c6e52be57cbf55c635be3b45630
Storm Dragon <stormdragon2976@gmail.com>
2019-03-05 18:26:11 -0500

    Added help display for the man page.

---
+++ src/dragonfilemanager/commands/application/display_help.py
@@ -0,0 +1,31 @@
+
+import shlex
+
+class command():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
+        self.startUpManager = self.dragonfmManager.getStartUpManager()
+        self.viewManager = None
+    def shutdown(self):
+        pass
+    def getName(self):
+        return _('Display Help')
+    def getDescription(self):
+        return _('Documentation for DragonFM')
+    def active(self):
+        return True
+    def getValue(self):
+        return None
+    def getShortcut(self):
+        return "f1"
+    def run(self, callback = None):
+        if self.viewManager == None:
+            self.viewManager = self.dragonfmManager.getViewManager()
+        terminalcmd = self.settingsManager.get('application', 'commandline')
+        if terminalcmd == '':
+            return
+        self.startUpManager.start('man dragonfm')
+        if callback:
+            callback()
