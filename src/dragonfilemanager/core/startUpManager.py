1905378cb3f2c9c910e6847fdbcd67807127aa85
chrys <chrys@linux-a11y.org>
2019-03-06 13:03:02 +0100

    Update startUpManager.py

--- src/dragonfilemanager/core/startUpManager.py
+++ src/dragonfilemanager/core/startUpManager.py
@@ -1,12 +1,12 @@
 import sys, os, curses
 
-class startUpManager():
+class processManager():
     def __init__(self, dragonfmManager):
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
 
-    def start(self, application = '', pwd = ''):
+    def startExternal(self, application = '', pwd = ''):
         if application == '':
             return
         if pwd != '' and os.access(pwd, os.R_OK):
