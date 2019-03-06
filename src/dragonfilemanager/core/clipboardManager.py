627eb03f8b23e50fc89c343d8dfd5fa4fce713a4
chrys <chrys@linux-a11y.org>
2019-03-06 14:45:06 +0100

    Update clipboardManager.py

--- src/dragonfilemanager/core/clipboardManager.py
+++ src/dragonfilemanager/core/clipboardManager.py
@@ -5,3 +5,13 @@ class clipboardManager():
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
+        self.clipboard = {}
+    def setClipboard(self, selection):
+        if selection == None:
+            self.clearClipboard()
+        if selection == {}:
+            self.clearClipboard()            
+        self.clipboard = selection.copy()
+    def clearClipboard(self):
+        self.clipboard = {}
+        
