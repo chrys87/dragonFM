029b2ca6aa1f4ab12f17bf7bb1312b5c49f1c05b
chrys <chrys@linux-a11y.org>
2019-03-06 13:15:30 +0100

    Update processManager.py

--- src/dragonfilemanager/core/processManager.py
+++ src/dragonfilemanager/core/processManager.py
@@ -5,15 +5,29 @@ class processManager():
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-
+        self.externalStarted = False
+        self.internalProcesses = {}
+    def startInternal(self, type, description = '', process = None, postProcess = None):
+        if process == None:
+            return None
+    def getNewProcessID(self):
+        return 1
+    def stopInternal(self, id):
+        pass
+    def isExternalStarted(self):
+        return self.externalStarted
+    def setExternalStarted(self, isExternalStarted):
+        self.externalStarted = isExternalStarted
     def startExternal(self, application = '', pwd = ''):
         if application == '':
             return
         if pwd != '' and os.access(pwd, os.R_OK):
             os.chdir(pwd)
         self.dragonfmManager.leave()
+        self.setExternalStarted(True)
         try:
             os.system(application)
         except:
             pass
+        self.setExternalStarted(False)
         self.dragonfmManager.enter()
