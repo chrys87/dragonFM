0da8f3166d280f29887424bcc76dda33cdcc9c78
chrys <chrys@linux-a11y.org>
2019-03-06 15:06:22 +0100

    Update selectionManager.py

--- src/dragonfilemanager/core/selectionManager.py
+++ src/dragonfilemanager/core/selectionManager.py
@@ -19,7 +19,7 @@ class selectionManager():
         if selection == []:
             currentCursor = self.getCursorCurrentTab()
             if currentCursor != None:
-                currentSelection = [currentCursor]
+                currentSelection = [currentCursor].copy()
         else:
             currentSelection = selection
         return currentSelection 
