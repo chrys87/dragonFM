9804184a92f0fe8bb92ec6f22cbef4c5372e73eb
chrys <chrys@linux-a11y.org>
2019-03-06 14:58:52 +0100

    Update selectionManager.py

--- src/dragonfilemanager/core/selectionManager.py
+++ src/dragonfilemanager/core/selectionManager.py
@@ -8,6 +8,18 @@ class selectionManager():
         self.viewManager = self.dragonfmManager.getViewManager()
         
     def getSelectionCurrentTab(self):
-        return []
+        return [].copy()
     def getCursorCurrentTab(self):
         return None
+    def getSelectionOrCursorCurrentTab(self):
+        # use active cursor if no selection is done.
+        # return list of file elements or None if no elements exists
+        currentSelection = None
+        selection = self.getSelectionCurrentTab()
+        if selection == []:
+            currentCursor = self.getCursorCurrentTab()
+            if currentCursor != None:
+                currentSelection = [currentCursor]
+        else:
+            currentSelection = selection
+        return currentSelection 
