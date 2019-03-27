a7819bdc24a1fc528e162b54867bec0c4dfb1c59
Storm Dragon <stormdragon2976@gmail.com>
2019-03-27 08:30:37 -0400

    Revised delete.py. Should work this time.

--- src/dragonfilemanager/commands/view/delete.py
+++ src/dragonfilemanager/commands/view/delete.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 
-import dragonFM
 import os, shutil
 
-# Get the files and directories that were selected.
-selected = selectionManager.getSelectionOrCursorCurrentTab()
-
-# Loop through the files and directories and delete them.
-for i in selected:
-    if os.is_file(i):
-        os.remove(i)
-    if os.is_dir(i):
-        shutil.rmtree(i)
-
-
+class command():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
+    def shutdown(self):
+        pass
+    def getName(self):
+        return _('No description found')
+    def getDescription(self):
+        return _('No description found')
+    def active(self):
+        return True
+    def getValue(self):
+        return None
+    def getShortcut(self):
+        return None
+    def run(self, callback = None):
+        # Get the files and directories that were selected.
+        selected = selectionManager.getSelectionOrCursorCurrentTab()
+                                                                                                                                                                
+        # Loop through the files and directories and delete them.
+        for i in selected:
+            if os.is_file(i):
+                os.remove(i)
+            if os.is_dir(i):
+                shutil.rmtree(i)
+        if callback:
+          callback()
