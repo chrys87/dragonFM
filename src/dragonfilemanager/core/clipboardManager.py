1ff7352a94ba5cfa360a48c28e40143e4a489cb7
Chrys <chrys@linux-a11y.org>
2019-04-26 00:17:03 +0200

    add operation to clipboardManager

--- src/dragonfilemanager/core/clipboardManager.py
+++ src/dragonfilemanager/core/clipboardManager.py
@@ -6,14 +6,18 @@ class clipboardManager():
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
         self.clipboard = []
-    def setClipboard(self, selection):
+        self.operation = '' # copy, cut
+    def setClipboard(self, selection, operation = 'copy'):
         if selection == None:
             return 
         self.clipboard = selection.copy()
+        self.operation = operation
     def clearClipboard(self):
         self.clipboard = []
+        self.operation = ''
     def getClipboard(self):
         return self.clipboard.copy()
+    def getOperation(self):
+        return self.operation
     def isClipboardEmpty(self):
         return self.getClipboard() == []
-        
