6ee2ca6b01c23616d23c1364c2abd64b67c15470
Chrys <chrys@linux-a11y.org>
2019-04-26 21:59:20 +0200

    add select toggle

---
+++ src/dragonfilemanager/commands/folder/select/toggle_selection_curr_entry.py
@@ -0,0 +1,27 @@
+class command():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
+    def shutdown(self):
+        pass
+    def getName(self):
+        return _('Previous Entry')
+    def getDescription(self):
+        return _('Move Cursor to previous entry')
+    def active(self):
+        return True
+    def getValue(self):
+        return None
+    def getShortcut(self):
+        return None
+    def run(self, callback = None):
+        folderManager = self.dragonfmManager.getCurrFolderManager()
+        if folderManager.isCurrentEntrySelected():
+            folderManager.uncselectCurrentEntry()
+        else:    
+            folderManager.selectCurrentEntry()
+        folderManager.setNeedRefresh()
+        if callback:
+            callback()
