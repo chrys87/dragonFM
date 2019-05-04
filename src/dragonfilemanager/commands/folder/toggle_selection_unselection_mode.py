a4e8b44003d39359748c84abfc36a9bd9f760b14
Chrys <chrys@linux-a11y.org>
2019-05-04 23:54:45 +0200

    add selection / unselection toggle

---
+++ src/dragonfilemanager/commands/folder/toggle_selection_unselection_mode.py
@@ -0,0 +1,26 @@
+class command():
+    def __init__(self, dragonfmManager):
+        self.dragonfmManager = dragonfmManager
+        self.dragonfmManager = dragonfmManager
+        self.screen = self.dragonfmManager.getScreen()
+        self.settingsManager = self.dragonfmManager.getSettingsManager()
+    def shutdown(self):
+        pass
+    def getName(self):
+        return _('toggle selection / unselection')
+    def getDescription(self):
+        return _('toggle selection / unselection on navigation')
+    def active(self):
+        return True
+    def getValue(self):
+        return None
+    def getShortcut(self):
+        return None
+    def run(self, callback = None):
+        folderManager = self.dragonfmManager.getCurrFolderManager()
+        if folderManager.getSelectionMode() == 0:
+            folderManager.setSelectionMode(1)
+        else:
+            folderManager.setSelectionMode(0)
+        if callback:
+            callback()
