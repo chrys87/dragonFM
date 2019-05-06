887b34b6d84a66118bce76cf494f1b6a902be839
chrys <chrys@linux-a11y.org>
2019-05-06 16:44:38 +0200

    Update delete.py

--- src/dragonfilemanager/commands/folder/delete.py
+++ src/dragonfilemanager/commands/folder/delete.py
@@ -24,7 +24,7 @@ class command():
         # Get the files and directories that were selected.
         selection = self.selectionManager.getSelectionOrCursorCurrentTab()
                                                                                                                                                                 
-        self.fileManager.deleteSelection(selection)
+        self.fileManager.createDeleteSelectionThread(selection)
         if folderManager.getSelectionMode() != 0:
             folderManager.setSelectionMode(0)
 
