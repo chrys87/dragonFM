2123721f54e4d3bbad48a26401aabb0e7ab6131a
chrys <chrys@linux-a11y.org>
2019-05-07 06:53:10 +0200

    Update inputManager.py

--- src/dragonfilemanager/core/inputManager.py
+++ src/dragonfilemanager/core/inputManager.py
@@ -5,14 +5,29 @@ class inputManager():
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-    #def getKey(self):
     def get(self):
+        if not self.screen:
+            return None
+        keys = ''
+        key = self.screen.getch()
+        if key == curses.ERR:
+            return None
+        keys = curses.keyname(key).decode("utf-8") 
+        self.screen.nodelay(True)
+        while True:
+            key = self.screen.getch()
+            if key == curses.ERR:
+                break
+            key = curses.keyname(key).decode("utf-8") 
+            keys += ',{0}'.format(key)
+        self.screen.nodelay(False)
+        return str(keys)        
+    def getKey(self):
         if not self.screen:
             return None
         key = self.screen.getch()
         key = curses.keyname(key).decode("utf-8") 
         return key
-    #def get(self):    
     def getSequenceAsList(self):
         if not self.screen:
             return None
@@ -22,7 +37,6 @@ class inputManager():
         key = self.screen.getch()
         keys.append(chr(key))
         self.screen.nodelay(True)
-        wait = time.time()
         while True:
             key = self.screen.getch()
             if key == curses.ERR:
