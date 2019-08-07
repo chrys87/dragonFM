1a159f7c76e5ccb8cbbe45aba82c37ea393e2603
Chrys <chrys@linux-a11y.org>
2019-08-07 21:58:17 +0200

    add more visibility

--- src/dragonfilemanager/commands/listManager-available/paste.py
+++ src/dragonfilemanager/commands/listManager-available/paste.py
@@ -8,18 +8,8 @@ class command(baseCommand):
         self.fileManager = self.dragonfmManager.getFileManager()
         self.setName('Paste')
         self.setDescription('Paste the clipboard to current location')
-    def shutdown(self):
-        pass
     def active(self):
-        return True
-    def visible(self):
-        return True
-    def getValue(self):
-        return None
-    def getShortcut(self):
-        return None
-    def active(self):
-        return self.commandManager.isCommandValidForFileOperation(minSelection = 1)
+        return self.commandManager.isCommandValidForFileOperation(writePerm = True)
     def run(self, callback = None):   
         self.fileManager.spawnPasteClipboardThread()
         if callback:
