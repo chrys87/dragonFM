c2a9eeef0164df5c517c52eddf4f3064c633a423
Storm Dragon <stormdragon2976@gmail.com>
2019-08-08 17:40:10 -0400

    Updated ignore_selection

--- src/dragonfilemanager/commands/listManager-available/ignore_selection.py
+++ src/dragonfilemanager/commands/listManager-available/ignore_selection.py
@@ -1,23 +1,16 @@
-class command():
+from dragonfilemanager.core.baseCommand import baseCommand
+
+class command(baseCommand):
     def __init__(self, dragonfmManager):
+        baseCommand.__init__(self, dragonfmManager)
         self.dragonfmManager = dragonfmManager
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-    def shutdown(self):
-        pass
-    def getName(self):
-        return _('Start Ignore Selection Mode')
-    def getDescription(self):
-        return _('Keep selection on navigation')
+        self.setName('Start Ignore Selection Mode')
+        self.setDescription('Keep selection on navigation')
     def active(self):
-        return True
-    def visible(self):
-        return True
-    def getValue(self):
-        return None
-    def getShortcut(self):
-        return None
+        return self.commandManager.isCommandValidForFileOperation(minSelection = 1)
     def run(self, callback = None):
         listManager = self.dragonfmManager.getCurrListManager()
         listManager.setSelectionMode(2)
