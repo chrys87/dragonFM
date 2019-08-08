fb6b67b07dabbf3b0574429af38e1d27aacfa25b
Storm Dragon <stormdragon2976@gmail.com>
2019-08-08 16:22:46 -0400

    Updated prevpage

--- src/dragonfilemanager/commands/listManager-available/prev_page.py
+++ src/dragonfilemanager/commands/listManager-available/prev_page.py
@@ -1,22 +1,15 @@
-class command():
+from dragonfilemanager.core.baseCommand import baseCommand
+
+class command(baseCommand):
     def __init__(self, dragonfmManager):
+        baseCommand.__init__(self, dragonfmManager)
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-    def shutdown(self):
-        pass
-    def getName(self):
-        return _('Previous Page')
-    def getDescription(self):
-        return _('Goto previous Page')
+        self.setName('Previous Page')
+        self.setDescription('Goto previous Page')
     def active(self):
-        return True
-    def visible(self):
-        return True
-    def getValue(self):
-        return None
-    def getShortcut(self):
-        return None
+        return self.commandManager.isCommandValidForFileOperation()
     def run(self, callback = None):
         listManager = self.dragonfmManager.getCurrListManager()
         listManager.prevPage()
