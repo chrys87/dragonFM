76b999a36c3ffbfea015443985fc994b4bf7fa9b
Storm Dragon <stormdragon2976@gmail.com>
2019-08-08 16:00:11 -0400

    updated toggle_selecttion_unselectmode

--- src/dragonfilemanager/commands/listManager-available/toggle_selection_unselection_mode.py
+++ src/dragonfilemanager/commands/listManager-available/toggle_selection_unselection_mode.py
@@ -1,22 +1,15 @@
-class command():
+from dragonfilemanager.core.baseCommand import baseCommand
+
+class command(baseCommand):
     def __init__(self, dragonfmManager):
+        baseCommand.__init__(self, dragonfmManager)
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
-    def shutdown(self):
-        pass
-    def getName(self):
-        return _('toggle selection / unselection')
-    def getDescription(self):
-        return _('toggle selection / unselection on navigation')
+        self.setName('toggle selection / unselection')
+        self.setDescription('toggle selection / unselection on navigation')
     def active(self):
-        return True
-    def visible(self):
-        return True
-    def getValue(self):
-        return None
-    def getShortcut(self):
-        return None
+        return self.commandManager.isCommandValidForFileOperation()
     def run(self, callback = None):
         listManager = self.dragonfmManager.getCurrListManager()
         if listManager.getSelectionMode() == 0:
