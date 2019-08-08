3cf7f583bbeec4219b0a8aae1792e83ddebbde3a
Storm Dragon <stormdragon2976@gmail.com>
2019-08-08 17:37:58 -0400

    Update goto root

--- src/dragonfilemanager/commands/listManager-available/goto_root.py
+++ src/dragonfilemanager/commands/listManager-available/goto_root.py
@@ -1,25 +1,17 @@
+from dragonfilemanager.core.baseCommand import baseCommand
 from os.path import expanduser
 
-class command():
+class command(baseCommand):
     def __init__(self, dragonfmManager):
+        baseCommand.__init__(self, dragonfmManager)
         self.dragonfmManager = dragonfmManager
         self.screen = self.dragonfmManager.getScreen()
         self.settingsManager = self.dragonfmManager.getSettingsManager()
         self.viewManager = None
-    def shutdown(self):
-        pass
-    def getName(self):
-        return _('Goto Root')
-    def getDescription(self):
-        return _('Goto root folder')
+        self.setName('Goto Root')
+        self.setDescription('Goto root folder')
     def active(self):
-        return True
-    def visible(self):
-        return True
-    def getValue(self):
-        return None
-    def getShortcut(self):
-        return None
+        return self.commandManager.isCommandValidForFileOperation()
     def run(self, callback = None):
         listManager = self.dragonfmManager.getCurrListManager()
         listManager.gotoFolder('/')
